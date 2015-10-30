52a52644f712b7c6a809a83fc886d3e89870bbe9
Manuel Limones <limonesu.me@gmail.com>
2015-10-30 05:53:20 -0600

    Fix broken keil proyect

--- mdk/RTE/RTE_Components.h
+++ mdk/RTE/RTE_Components.h
@@ -3,7 +3,7 @@
  * Auto generated Run-Time-Environment Component Configuration File
  *      *** Do not modify ! ***
  *
- * Project: 'Cpp_03_RTOS' 
+ * Project: 'MA_02_Clock' 
  * Target:  'Target 1' 
  */
 
